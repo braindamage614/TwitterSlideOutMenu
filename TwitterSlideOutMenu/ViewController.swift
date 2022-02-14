4ae5da543c689442ecef0e6bb5d9ea12ac24ca6d
Yuanjun Li <liyuan@pwr-craft.com>
2022-02-14 23:04:09 +0800

    Initial Commit

---
+++ TwitterSlideOutMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterSlideOutMenu
+//
+//  Created by braindamage on 2022/2/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
